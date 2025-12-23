fb19cd93b2b92692b590e5e7d9ce477cb8ab7ab9
psh2135 <s026635836@gmail.com>
2025-12-23 22:13:20 +0200

    more validation at DTO

--- ChineseSaleApi/DTO/PackageDto.cs
+++ ChineseSaleApi/DTO/PackageDto.cs
@@ -1,9 +1,17 @@
-﻿namespace ChineseSaleApi.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChineseSaleApi.DTO
 {
     public class CreatePackageDto
     {
+        [Required]
+        [MaxLength(20)]
         public string Name { get; set; } = null!;
+        [Required]
+        [Range(20, 9000)]
         public decimal Price { get; set; }
+        [Required]
+        [Range(20, 9000)]
         public int TicketsCount { get; set; }
     }
     public class PackageDto
