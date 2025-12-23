27f16088e18abc73f5a3453739d51814a983ead6
psh2135 <s026635836@gmail.com>
2025-12-23 20:26:11 +0200

    add validation to DTO

--- ChineseSaleApi/DTO/GiftDto.cs
+++ ChineseSaleApi/DTO/GiftDto.cs
@@ -1,18 +1,23 @@
-﻿namespace ChineseSaleApi.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChineseSaleApi.DTO
 {
     public class CreateGiftDto
     {
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
         public string Title { get; set; } = null!;
+        [Required]
+        [StringLength(250, MinimumLength = 2)]
         public string Description { get; set; } = null!;
+        [Required]
         public int DonorId { get; set; }
-        public DateTime EndTime { get; set; }
     }
     public class GiftDto
     {
         public int Id { get; set; }
         public string Title { get; set; } = null!;
         public string Description { get; set; } = null!;
-        public string Status { get; set; } = null!;
     }
 
 }
